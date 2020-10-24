fe40dcb01710e1b3e6ef4a4840ab39699fdb8044
poulstar <info@poulstar.com>
2020-10-24 17:34:10 +0330

    jalase aval 3ta tamrin hal kardim

---
+++ Term 2/1-1-problem.py
@@ -0,0 +1,9 @@
+radius = 7
+area = 3.14 * radius * radius
+premeter = 3.14 * 2 * radius
+
+msg1 = 'Area of circle is:'
+msg2 = 'Premeter of circle is:'
+
+print(msg1, area)
+print(msg2, premeter)
---
+++ Term 2/1-2-problem.py
@@ -0,0 +1,9 @@
+side = 7
+area =  side * side
+premeter = side * 4
+
+msg1 = 'Area of Square is:'
+msg2 = 'Premeter of Square is:'
+
+print(msg1, area)
+print(msg2, premeter)
